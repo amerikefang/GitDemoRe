17897267be3f4b17a2faf862710f335b57412c9e
Kyle Fang <yzuo@panzura.com>
2019-09-07 12:45:46 -0700

    Add method for send UDP

--- seleniumdemo/portscanThreading.py
+++ seleniumdemo/portscanThreading.py
@@ -9,7 +9,7 @@ def portscan(port):
     try:
         con = s.connect((target, port))
         with print_lock:
-            print('port', port, 'is open!!!!!!!')
+            print('port', port, 'is open!!!!!!!!!!!')
         con.close()
 
     except:
